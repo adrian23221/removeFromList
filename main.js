e961f5dc11ba60199eb8bd93526a3857f33fe8b7
Adrian Pruszkowski <pruszkowski.adrian@gmail.com>
2019-01-24 05:56:35 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const removeTask = (e) => {
+    // e.target.parentNode.remove();
+    // console.log(e.target.parentNode);
+
+    // e.target.parentNode.style.textDecoration = "line-through";
+    // e.target.remove();
+
+    const index = e.target.dataset.key;
+    document.querySelector(`[data-key='${index}']`).remove();
+}
+
+document.querySelectorAll('button(data-key').forEach(item =>
+    item.addEventListener('click', removeTask))
